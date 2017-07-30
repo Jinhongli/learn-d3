f50d3a8052c51bdd4c452a1a6b68a7d4fca0e158
Jin Hongli <jhl211@163.com>
2017-07-30 15:19:02 +0800

    basic histogram

--- src/app/component/DashBoard.js
+++ src/app/component/DashBoard.js
@@ -18,7 +18,7 @@ chance.mixin({
 chance.mixin({
     'student': function() {
         return {
-            name: chance.name(),
+            name: chance.first(),
             scores: chance.scores()
         }
     }
@@ -34,15 +34,14 @@ glue.define('component.DashBoard', {
         this.svg.selection = d3.select(this.svg.node);
         this.svg.width = this.svg.$.width();
         this.svg.height = this.svg.$.height();
-        this.svg.padding = 20;
+        this.svg.padding = 30;
         this.subject = 'math';
-        this.students = this.getStudentName();
-        this.subjectData = this.getSubjectData(this.subject);
-        this.drawAxis();
+        this.draw(this.getSubjectData());
     },
     getSubjectData: function(subject){
-        var subjectData = [];
-        for(var i=0,l=data.length; i<l; i++){
+        subject = this.subject;
+        let subjectData = [];
+        for(let i=0,l=data.length; i<l; i++){
             subjectData.push({
                 name: data[i].name,
                 score: data[i].scores[subject]
@@ -52,38 +51,70 @@ glue.define('component.DashBoard', {
         return subjectData;
     },
     getStudentName: function(){
-        var names = [];
-        for(var i=0,l=data.length; i<l; i++){
-            names.push({
-                name: data[i].name,
-            });
+        let names = [];
+        for(let i=0,l=data.length; i<l; i++){
+            names.push(data[i].name);
         }
         return names;
     },
-    drawData: function(){
-        let update = this.svg.selection.selectAll('rect').data(this.subjectData);
-        let enter = update.enter();
+    draw: function(subjectData){
+        this.svg.selection.selectAll('g.axis').remove();
+        let students = this.getStudentName();
+        let yTranslate = this.svg.height - this.svg.padding;
 
-        enter.append('rect')
-            .attr('x', function(d, i){ return i*40})
-            .attr('y', function(d){ return this.svg.height - this.svg.padding - d.score }.bind(this))
-            .attr('width', '20')
-            .attr('height', function(d){ return d.score });
-    },
-    drawAxis: function(){
-        var yTranslate = this.svg.height - this.svg.padding;
-        var xScale = d3.scaleLinear().domain([0,100]).range([this.svg.padding, this.svg.width - 2*this.svg.padding]);
-        var xAxis = d3.axisBottom(xScale);
+        let xScale = d3.scaleBand().domain(d3.range(0, students.length)).range([this.svg.padding, this.svg.width - 2*this.svg.padding]).padding(0.3);
+        let yScale = d3.scaleLinear().domain([0,100]).range([0, this.svg.height - 2*this.svg.padding]);
+
+        let update = this.svg.selection.selectAll('g').data(subjectData);
+        let enter = update.enter();
+        let exit = update.exit();
+
+        // Update pattern
+        update.select('rect')
+            .attr('x', function(d, i){ return xScale(i)}.bind(this))
+            .attr('y', function(d){ return this.svg.height - this.svg.padding - yScale(d.score) }.bind(this))
+            .attr('width', xScale.bandwidth())
+            .attr('height', function(d){ return yScale(d.score) }.bind(this));
+        update.select('text').text(function(d){return d.score})
+            .attr('x', function(d, i){ return xScale(i)}.bind(this))
+            .attr('y', function(d){ return this.svg.height - this.svg.padding - yScale(d.score) }.bind(this))
+            .attr('dx', 15)
+            .attr('dy', -10);
+
+        // Enter pattern
+        let enterGroup = enter.append('g');
+        enterGroup.append('rect')
+            .attr('x', function(d, i){ return xScale(i)}.bind(this))
+            .attr('y', function(d){ return this.svg.height - this.svg.padding - yScale(d.score) }.bind(this))
+            .attr('width', xScale.bandwidth())
+            .attr('height', function(d){ return yScale(d.score) }.bind(this));
+        enterGroup.append('text').text(function(d){return d.score})
+            .attr('x', function(d, i){ return xScale(i)}.bind(this))
+            .attr('y', function(d){ return this.svg.height - this.svg.padding - yScale(d.score) }.bind(this))
+            .attr('dx', 15)
+            .attr('dy', -10)
+
+        // Exit pattern
+        exit.remove();
+
+        // draw x-axis
+        let xAxis = d3.axisBottom(xScale).tickFormat(function(d, i){return students[i]});
         this.svg.selection.append('g').call(xAxis)
-            .attr('transform', 'translate(0, '+ yTranslate +')');
+            .attr('transform', 'translate(0, '+ yTranslate +')')
+            .classed('axis', true);
+        // draw y-axis
+        yScale.range([this.svg.height - 2*this.svg.padding, 0]);
+        let yAxis = d3.axisLeft(yScale);
+        this.svg.selection.append('g').call(yAxis)
+            .attr('transform', 'translate('+ this.svg.padding +','+ this.svg.padding +')')
+            .classed('axis', true);
     },
     listeners: {
         click: function (event, element, elementType) {
             if(elementType === 'subjectBtn'){
-                var subject = $(event.target).data('subject');
-                console.log(subject);
+                let subject = $(event.target).data('subject');
                 this.subject = subject;
-                this.getSubjectData(subject);
+                this.draw(this.getSubjectData());
             }
         }
     }
